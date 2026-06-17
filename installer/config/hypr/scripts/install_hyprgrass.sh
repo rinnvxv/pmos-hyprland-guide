d2485ab63faafc868b5e74e081b4f980e156cea4
rinnvxv <66520964+rinnvxv@users.noreply.github.com>
2026-06-17 17:19:25 +0900

    Update install_hyprgrass.sh

--- installer/config/hypr/scripts/install_hyprgrass.sh
+++ installer/config/hypr/scripts/install_hyprgrass.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+echo "Installing hyprgrass..."
 hyprpm update
 hyprpm add https://github.com/horriblename/hyprgrass
 hyprpm enable hyprgrass
+echo "hyprgrass installed! This window will close in 3 seconds."
+sleep 3
 
 sed -i '/install_hyprgrass\.sh/d' ~/.config/hypr/hyprland.conf
 
